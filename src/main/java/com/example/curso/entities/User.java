e1a9e492cfb8e5abf17bda678fa738c2f3fc3932
Gabriel Bessa <bessagabriel490@gmail.com>
2021-01-23 17:57:53 -0300

    JPA Repositorio, injeção de dependência, inserção no H2!

--- src/main/java/com/example/curso/entities/User.java
+++ src/main/java/com/example/curso/entities/User.java
@@ -14,7 +14,7 @@ public class User implements Serializable{
 
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String nome;
 	private String email;
 	private String phone;
@@ -23,7 +23,7 @@ public class User implements Serializable{
 	public User() {
 	}
 
-	public User(long id, String nome, String email, String phone, String password) {
+	public User(Long id, String nome, String email, String phone, String password) {
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
@@ -31,7 +31,7 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
