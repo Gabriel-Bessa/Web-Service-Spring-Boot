913f0ee73d9794efdb85b9ab3a9dfe741a4d12d7
Gabriel Bessa <bessagabriel490@gmail.com>
2021-01-26 16:19:26 -0300

    Entidade Payment, associação um para um!

--- src/main/java/com/example/curso/config/TestConfig.java
+++ src/main/java/com/example/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.curso.entities.Category;
 import com.example.curso.entities.Order;
 import com.example.curso.entities.OrderItem;
+import com.example.curso.entities.Payment;
 import com.example.curso.entities.Product;
 import com.example.curso.entities.User;
 import com.example.curso.entities.enums.OrderStatus;
@@ -19,7 +20,6 @@ import com.example.curso.repositories.OrderItemRepository;
 import com.example.curso.repositories.OrderRepository;
 import com.example.curso.repositories.ProductRepository;
 import com.example.curso.repositories.UserRepository;
-import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 @Configuration
 @Profile("test")
@@ -83,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		ordemItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
--- src/main/java/com/example/curso/entities/Product.java
+++ src/main/java/com/example/curso/entities/Product.java
@@ -128,4 +128,5 @@ public class Product implements Serializable {
 		return true;
 	}
 
+
 }
